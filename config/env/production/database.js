dd55add69cd23a2694b15f8956e2241c82ce2ac1
sakhawat9 <sakhawathossain7969@gmail.com>
2022-12-30 09:49:55 +0600

    add pg

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,17 +1,25 @@
+const parse = require("pg-connection-string").parse;
+const config = parse(process.env.DATABASE_URL);
+
 module.exports = ({ env }) => ({
-  connection: {
-    client: "postgres",
-    connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "strapi"),
-      user: env("DATABASE_USERNAME", "strapi"),
-      password: env("DATABASE_PASSWORD", "strapi"),
-      schema: env("DATABASE_SCHEMA", "public"), // Not required
-      ssl: {
-        ca: env("DATABASE_CA")
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "bookshelf",
+      settings: {
+        client: "postgres",
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        username: config.user,
+        password: config.password,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      },
+      options: {
+        ssl: true,
       },
     },
-    debug: false,
   },
 });
